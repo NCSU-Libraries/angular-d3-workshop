3f4c9f1586bdab9790988529c71c20abc0aa688b
Todd Stoffer <tdstoffe@ncsu.edu>
2018-02-01 16:06:03 -0500

    ch ch ch changes

--- src/app/fire-data.service.ts
+++ src/app/fire-data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { catchError, map, tap } from 'rxjs/operators';
 
 import { Fire } from './data.interface';
 
@@ -14,11 +13,13 @@ export class FireDataService {
   private fireDataUrl = 'api/fires'; // URL to in-memory fires data
 
   constructor(private http: HttpClient) { }
-
+  //Observable: an array of data that can change
   getData(): Observable<Fire[]> {
     return this.http.get<Fire[]>(this.fireDataUrl);
   }
+  // If we were retrieving data from a 'live' API that we wanted to update - we would have an update function here
 
+  //CRUD - like delete
   deleteFire(fire: Fire): Observable<Fire> {
     return this.http.delete<Fire>(`${this.fireDataUrl}/${fire.id}`, httpOptions);
   }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -9,16 +9,18 @@ import { Fire } from '../data.interface';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  //This is where we introduce data interface
   private fires: Fire[];
   private deleteFire: Fire;
   constructor(private fireDataService: FireDataService) { }
-
+  //Lifecycle Hooks - after first ngOnChanges
   ngOnInit() {
      this.getFireData();
   }
 
   getFireData(): void {
     this.fireDataService.getData()
+    //Subscribe is the other half of the observable. Observable is newspaper, subscribe is what gets it. It is also only delivered at the newspapers set delivery cycle
       .subscribe(fires => this.fires = fires);
   }
 
